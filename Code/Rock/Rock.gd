529f2a029fa1dae9f53bd21c68c6683e6a3b268a
LegoNinja25 <lego.ninja.0025@gmail.com>
2026-02-09 16:26:50 -0800

    Created Art
    Put art on bullet
    put art and code for random rock
    created art for ship and thrust

--- Code/Rock/Rock.gd
+++ Code/Rock/Rock.gd
@@ -1,5 +1,7 @@
 extends ScreenWrapper
 class_name Rock
+@onready var sprites: AnimatedSprite2D = $Sprites
+
 
 var main: Main
 var velocity: Vector2 = Vector2.ZERO # pixles / sec
@@ -10,6 +12,7 @@ var velocity: Vector2 = Vector2.ZERO # pixles / sec
 func _ready() -> void:
 	#velocity = Vector2(0,200) #test speed
 	velocity = Vector2(randf_range(-speed, speed),randf_range(-speed, speed))
+	sprites.frame = randi_range(0,2)
 
 
 func _process(delta: float) -> void:
